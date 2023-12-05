a4a39c7d7198796247a807d45ee4090fc863f395
borgepl <pdeborger@gmail.com>
2023-12-05 12:28:21 +0100

    Search Banner

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { MenuItemsList } from '../Components/Page/MenuItems'
+import { Banner } from '../Components/Page/Common'
 
 export default function Home() {
   return (
     (<div>
+        <Banner />
         <div className='container p-2'>
             <MenuItemsList/>
         </div>
