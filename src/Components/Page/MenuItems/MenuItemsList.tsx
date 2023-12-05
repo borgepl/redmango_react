825c09c28f78128aadeca61c644a12b27f4083c9
borgepl <pdeborger@gmail.com>
2023-12-05 13:59:27 +0100

    Search function for Banner

--- src/Components/Page/MenuItems/MenuItemsList.tsx
+++ src/Components/Page/MenuItems/MenuItemsList.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { menuItemModel } from '../../../Interfaces';
 import MenuItemCard from './MenuItemCard';
 import { useGetMenuItemsQuery } from '../../../Apis/menuItemApi';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setMenuItem } from '../../../Redux/menuItemSlice';
 import { MainLoader } from '../Common';
+import { RootState } from '../../../Redux/store';
 
 
 export default function MenuItemsList() {
 
-//const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
 
   // useEffect(() => {
   //   fetch("https://localhost:7052/api/MenuItem").then((response) => response.json())
@@ -22,11 +23,38 @@ export default function MenuItemsList() {
   const {data, isLoading} = useGetMenuItemsQuery(null);
   const dispatch = useDispatch();
 
+  const searchValue = useSelector(
+    (state: RootState) => state.menuItemStore.search
+  );
+
+  useEffect(() => {
+    if (data && data.result) {
+      const tempMenuArray = handleFilters(searchValue);
+      setMenuItems(tempMenuArray);
+    }
+  }, [searchValue]);
+
   useEffect(() => {
     if(!isLoading){
-      dispatch(setMenuItem(data.result))
+      dispatch(setMenuItem(data.result));
+      setMenuItems(data.result);
     }
   },  []);
+
+  const handleFilters = (search: string) => {
+    let tempMenuItems = [...data.result];
+
+    //search functionality - search on item name
+    if (search) {
+      const tempSearchMenuItems = [...tempMenuItems];
+      tempMenuItems = tempSearchMenuItems.filter((item: menuItemModel) =>
+        item.name.toUpperCase().includes(search.toUpperCase())
+      );
+    }
+
+    return tempMenuItems;
+  };
+
 
   if (isLoading) {
     return <MainLoader />;
@@ -34,7 +62,7 @@ export default function MenuItemsList() {
 
   return (
     (<div className='container row'>
-        {data.result.length > 0  && data.result.map((menuItem : menuItemModel, index : number) => (
+        {menuItems.length > 0  && menuItems.map((menuItem : menuItemModel, index : number) => (
             <MenuItemCard menuItem={menuItem} key={index}/>
         ))}
     </div>)
