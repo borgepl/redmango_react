a9c88cc9a49695699e2504ed584091844bf01b91
borgepl <pdeborger@gmail.com>
2023-12-06 15:29:27 +0100

    Filter call to API

--- src/Components/Page/Order/AllOrders.tsx
+++ src/Components/Page/Order/AllOrders.tsx
@@ -1,16 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MainLoader } from "../Common";
 import OrderList from "./OrderList";
 import { useGetAllOrdersQuery } from "../../../Apis/orderApi";
 import withAdminAuth from "../../../HOC/withAdminAuth";
+import { SD_Status } from "../../../Utility/SD";
+import inputHelper from "../../../Helper/InputHelper";
+
+const filterOptions = [
+  "All",
+  SD_Status.CONFIRMED,
+  SD_Status.COMPLETED,
+  SD_Status.BEING_COOKED,
+  SD_Status.READY_FOR_PICKUP,
+  SD_Status.CANCELLED,
+];
 
 function AllOrders() {
-  const { data, isLoading } = useGetAllOrdersQuery("");
+  
+  const [filters, setFilters] = useState({ searchString: "", status: "" });
+
+  const [orderData, setOrderData] = useState([]);
+  const [apiFilters, setApiFilters] = useState({
+    searchString: "",
+    status: "",
+  });
+
+  //const {data, isLoading} = useGetAllOrdersQuery("");
+
+  const { data, isLoading } = useGetAllOrdersQuery({
+    ...(apiFilters && {
+      searchString: apiFilters.searchString,
+      status: apiFilters.status,
+    }),
+  });
+
+  const handleFilters = () => {
+   
+    setApiFilters({
+      searchString: filters.searchString,
+      status: filters.status,
+    });
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => 
+  {
+    const tempValue = inputHelper(e, filters);
+    setFilters(tempValue);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setOrderData(data.result);
+    }
+  }, [data]);
+  
   return (
     <>
       {isLoading && <MainLoader />}
       {!isLoading && (
-        <OrderList isLoading={isLoading} orderData={data.result} />
+        <>
+          <div className="d-flex align-items-center justify-content-between mx-5 mt-3">
+            <h1 className="text-success">Orders List</h1>
+            <div className="d-flex" style={{ width: "40%" }}>
+              <input
+                type="text"
+                className="form-control mx-2"
+                name="searchString"
+                onChange={handleChange}
+                placeholder="Search Name, Email or Phone"
+              />
+             <select
+                className="form-select w-50 mx-2"
+                onChange={handleChange}
+                name="status"
+              >
+                {filterOptions.map((item, index) => (
+                  <option  key={index} value={item === "All" ? "" : item}>{item}</option>
+                ))}
+              </select>
+              <button className="btn btn-outline-success" onClick={handleFilters}>Filter</button>
+            </div>
+          </div>  
+          <OrderList isLoading={isLoading} orderData={orderData} />
+        </>  
       )}
     </>
   );
--- src/Pages/Order/MyOrders.tsx
+++ src/Pages/Order/MyOrders.tsx
@@ -6,18 +6,26 @@ import { useGetAllOrdersQuery } from "../../Apis/orderApi";
 import OrderList from "../../Components/Page/Order/OrderList";
 import { MainLoader } from "../../Components/Page/Common";
 
+
 function MyOrders() {
+
   const userId = useSelector((state: RootState) => state.userAuthStore.id);
-  const { data, isLoading } = useGetAllOrdersQuery(userId);
+  const { data, isLoading } = useGetAllOrdersQuery({userId});
   console.log(isLoading);
   console.log(data);
+
   return (
     <>
-     {isLoading && <MainLoader />}
+    {isLoading && <MainLoader />}
     {!isLoading && (
-     <OrderList isLoading={isLoading} orderData={data.result}/>
+      <>
+        <div className="d-flex align-items-center justify-content-between mx-5 mt-3">
+          <h1 className="text-success">My Orders</h1>
+        </div>  
+        <OrderList isLoading={isLoading} orderData={data.result} />
+      </>  
     )}
-    </>
+  </>
   );
 }
 
